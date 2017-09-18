4e63fd583e209b451c51fbd2324792385d7a1fe2
安富 孝司 <koji_micro@hotmail.co.jp>
2017-09-19 00:20:24 +0900

    first commit

---
+++ Logtter/Logtter/ViewModel/ViewModelTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logtter.ViewModel
+{
+    class ViewModelTemplate<T> : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+    }
+}
